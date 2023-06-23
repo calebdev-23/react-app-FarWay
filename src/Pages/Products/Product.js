08cc7689e861e39e107eddadf40da4904f3f1109
Your Name <you@example.com>
2023-06-23 09:26:53 +0300

    mise en place de l'illustration de notre produit

--- src/Pages/Products/Product.js
+++ src/Pages/Products/Product.js
@@ -1,9 +1,9 @@
-const Product = ({name, price, description}) =>{
-
+import image from "./../../assets/informatique.jpg"
+const Product = ({name, price, description, illustration}) =>{
     return (
         <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
-            <div className="card" style={{width : ""}}>
-                <img src="..." className="card-img-top" alt="..."/>
+            <div className="card shadow" style={{width : ""}} >
+                <img src={`http://localhost:8000/upload/products/${illustration}`} className="card-img-top" alt="..."/>
                     <div className="card-body">
                         <h5 className="card-title">{name}</h5>
                         <p className="card-text">{description}</p>
